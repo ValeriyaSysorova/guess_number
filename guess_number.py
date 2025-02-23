b99912d180901e7b20e48221a2351454e892f22a
Lera Sysorova <sysorova.lera@yandex.ru>
2025-02-23 19:18:37 +0300

    Change text 2.0

--- guess_number.py
+++ guess_number.py
@@ -6,9 +6,9 @@ print('Угадайте число от 1 до 100')
 while True:
     guess = int(input('Введите число: '))
     if guess < number:
-        print('Выше число меньше загаданного')
+        print('Выше число меньше загаданного(')
     elif guess > number:
-        print('Ваше число больше загаданного')
+        print('Ваше число больше загаданного(')
     elif guess == number:
         break
 
